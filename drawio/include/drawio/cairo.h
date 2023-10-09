49b0a7a3abd6ac1d2a3fee6c48226ac5768c7dd9
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-09 23:25:31 +0200

    Marking points with paint

--- drawio/include/drawio/cairo.h
+++ drawio/include/drawio/cairo.h
@@ -7,4 +7,4 @@ void drawio_line_draw(cairo_t *cairo, Point from, Point to, cairo_bool_t stroke)
 
 void drawio_circle_draw(cairo_t *cairo, Point center, double radius, cairo_bool_t stroke);
 
-void drawio_points_mark(cairo_t *cairo, Point *points, size_t count);
+void drawio_points_mark(cairo_t *cairo, Point *points, size_t count, u_int8_t r, u_int8_t g, u_int8_t b);
--- drawio/src/cairo.c
+++ drawio/src/cairo.c
@@ -21,11 +21,12 @@ void drawio_circle_draw(cairo_t *cairo, Point center, double radius, cairo_bool_
   }
 }
 
-void drawio_points_mark(cairo_t *cairo, Point *points, size_t count) {
+void drawio_points_mark(cairo_t *cairo, Point *points, size_t count, u_int8_t r, u_int8_t g, u_int8_t b) {
+  cairo_set_source_rgb(cairo, r, g, b);
+
   for (int i = 0; i < count; i++) {
     Point point = points[i];
-    drawio_circle_draw(cairo, point, 1, true);
+    cairo_arc(cairo, point.x, point.y, 3, 0, 2 * M_PI);
+    cairo_fill(cairo);
   }
-
-  cairo_stroke(cairo);
 }
