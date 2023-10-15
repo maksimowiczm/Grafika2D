e9e5d35874d839e38c09ea893099f5434e3bb15e
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-15 16:42:08 +0200

    Loading image from file

---
+++ src/context.c
@@ -0,0 +1,19 @@
+#include "context.h"
+
+#include "netpbm/PixMapReader.h"
+#include "stdlib.h"
+
+bool context_load_image(Context *context, const char *file_path, enum ImageType type) {
+    if (type == PPM) {
+        PixMapImage *image = netpbm_PixMap_read_from_file(file_path);
+        if (image == NULL) {
+            return false;
+        }
+        netpbm_convert_RGB_BGR(image);
+
+        free(context->image);
+        context->image = image;
+
+        return true;
+    }
+}
---
+++ src/include/context.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stdbool.h"
+
+enum ImageType {
+    PPM
+};
+
+typedef struct {
+    enum ImageType imageType;
+    void *image;
+} Context;
+
+bool context_load_image(Context *context, const char *file_path, enum ImageType type);
