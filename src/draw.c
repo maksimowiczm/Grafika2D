1e3639bf405bcc6cb2830bcd58930ddb041645ae
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-15 16:42:08 +0200

    Loading image from file

--- src/draw.c
+++ src/draw.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include "draw.h"
 
+static void set_up_bit_map(BITMAPINFOHEADER *bmih, BITMAPINFO *dbmi, PixMapHeader *header) {
+    // set up bit map
+    bmih->biSize = sizeof(*bmih);
+    bmih->biWidth = header->width;
+    bmih->biHeight = -header->height;
+    bmih->biPlanes = 1;
+    bmih->biBitCount = 8 * 3;
+    bmih->biCompression = BI_RGB;
+    bmih->biSizeImage = 0;
+    bmih->biXPelsPerMeter = 10;
+    bmih->biYPelsPerMeter = 10;
+    bmih->biClrUsed = 0;
+    bmih->biClrImportant = 0;
+
+    dbmi->bmiHeader = *bmih;
+    dbmi->bmiColors->rgbBlue = 0;
+    dbmi->bmiColors->rgbGreen = 0;
+    dbmi->bmiColors->rgbRed = 0;
+    dbmi->bmiColors->rgbReserved = 0;
+}
+
 void draw_pix_map_image(HWND hwnd, PixMapImage *image) {
+    if (image == NULL) {
+        return;
+    }
+
     PAINTSTRUCT ps;
     RECT r;
     GetClientRect(hwnd, &r);
@@ -9,58 +34,24 @@ void draw_pix_map_image(HWND hwnd, PixMapImage *image) {
         return;
     }
 
-    int windowWidth = r.right - r.left;
-    int windowHeight = r.bottom - r.top - 50;
+    // set up bit map
+    BITMAPINFOHEADER bmih = {0};
+    BITMAPINFO dbmi = {0};
+    PixMapHeader *header = (PixMapHeader *) image;
+    set_up_bit_map(&bmih, &dbmi, header);
+    void *bits = (void *) &(image->pixels[0]);
 
     HDC hdc = BeginPaint(hwnd, &ps);
-
-    PixMapHeader *header = (PixMapHeader *) image;
-    size_t length = header->width * header->height * 3;
-
-    // RGB ==> BGR
-    uint8_t *pixels = malloc(length);
-    memset(pixels, 0, length);
-    for (int i = 2; i < length; i += 3) {
-        // r -> b
-        pixels[i - 2] = image->pixels[i];
-        // r -> b
-        pixels[i] = image->pixels[i - 2];
-
-        pixels[i - 1] = image->pixels[i - 1];
-    }
-
-    // set up bit map
-    BITMAPINFOHEADER bmih = {
-            sizeof(BITMAPINFOHEADER),
-            header->width,
-            -header->height,
-            1,
-            8 * 3,
-            BI_RGB,
-            0,
-            10,
-            10,
-            0,
-            0
-    };
-
-    BITMAPINFO dbmi = {0};
-    dbmi.bmiHeader = bmih;
-    dbmi.bmiColors->rgbBlue = 0;
-    dbmi.bmiColors->rgbGreen = 0;
-    dbmi.bmiColors->rgbRed = 0;
-    dbmi.bmiColors->rgbReserved = 0;
-    void *bits = (void *) &(pixels[0]);
-
     HBITMAP hBitmap = CreateDIBSection(hdc, &dbmi, DIB_RGB_COLORS, &bits, NULL, 0);
     if (hBitmap == NULL) {
         printf("[-] Failed to create bitmap. Error code: %lu", GetLastError());
         return;
     }
-
-    memcpy(bits, pixels, length);
+    memcpy(bits, image->pixels, header->width * header->height * 3);
 
     // calculate destination size
+    int windowWidth = r.right - r.left;
+    int windowHeight = r.bottom - r.top - 50;
     int width = header->width, height = header->height;
     double aspect_ratio = (double) width / height;
     if (aspect_ratio > ((double) windowWidth / windowHeight)) {
@@ -82,5 +73,5 @@ void draw_pix_map_image(HWND hwnd, PixMapImage *image) {
                   DIB_RGB_COLORS,
                   SRCCOPY);
 
-    free(pixels);
+    EndPaint(hwnd, &ps);
 }
--- src/main.c
+++ src/main.c
@@ -1,34 +1,74 @@
 #include <windows.h>
 #include <stdio.h>
-#include "netpbm/PixMap.h"
-#include "netpbm/PixMapReader.h"
+#include <tchar.h>
 #include "inttypes.h"
 #include "draw.h"
+#include "context.h"
 
-//const char *file_path = "testy/ppm-test-01-p3.ppm";
-const char *file_path = "testy/ppm-test-02-p3-comments.ppm";
-//const char *file_path = "testy/ppm-test-07-p3-big.ppm";
-PixMapImage *image = NULL;
+Context *context;
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (image == NULL) {
-        image = netpbm_PixMap_read_from_file(file_path);
+static bool
+handle_button_load_click(HWND hwnd) {
+    OPENFILENAME ofn;
+    TCHAR szFile[260] = {0};
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hwnd;
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = _T("PPM\0*.ppm\0All\0*.*\0");
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileName(&ofn) == TRUE) {
+        const char *file_path = ofn.lpstrFile;
+        return context_load_image(context, file_path, PPM);
     }
 
+    if (strlen(ofn.lpstrFile) > 0) {
+        return false;
+    }
+
+    return true;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_PAINT:
-            draw_pix_map_image(hwnd, image);
+            draw_pix_map_image(hwnd, context->image);
             break;
         case WM_DESTROY:
             PostQuitMessage(0);
-            free(image);
             return 0;
+        case WM_COMMAND: {
+            bool loaded = handle_button_load_click(hwnd);
+
+            // redraw after load
+            if (loaded) {
+                RECT r;
+                GetClientRect(hwnd, &r);
+                InvalidateRect(hwnd, &r, TRUE);
+                break;
+            }
+
+            MessageBox(hwnd, "File type is not supported", "Error", MB_ICONERROR | MB_OK);
+        }
+            break;
         default:
             return DefWindowProcW(hwnd, msg, wParam, lParam);
     }
+
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PTSTR cmdline, int nCmdShow) {
+    Context main_context = {0};
+    context = &main_context;
+
     WNDCLASSW wc = {0};
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpszClassName = L"Draw Bitmap";
@@ -41,12 +81,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PTSTR cmdline, int 
     HWND hMainWindow = CreateWindowW(wc.lpszClassName, (LPCWSTR) L"Images",
                                      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                                      100, 100, 1280, 720, NULL, NULL, hInstance, NULL);
+    HWND hwndButton = CreateWindow(
+            "BUTTON",  // Predefined class; Unicode assumed
+            "Load file",      // Button text
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles
+            0,         // x position
+            0,         // y position
+            100,        // Button width
+            50,        // Button height
+            hMainWindow,     // Parent window
+            (HMENU) 10000,       // No menu.
+            (HINSTANCE) GetWindowLongPtr(hMainWindow, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
 
+    UpdateWindow(hMainWindow);
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
+    free(main_context.image);
     return (int) msg.wParam;
 }
