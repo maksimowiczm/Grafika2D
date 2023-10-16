07a7735771c3362f3fae1857f102877674bc7475
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-16 22:16:54 +0200

    Image abstraction 💀💀💀 (doesnt work XD)

--- src/include/context.hpp
+++ src/include/context.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
+#include <cinttypes>
+#include <vector>
+
 enum ImageType {
-  PPM
+  PPM,
+  JPG,
 };
 
 typedef struct {
+  int32_t width;
+  int32_t height;
+} ImageHeader;
+
+typedef struct {
+  ImageHeader header;
+  uint8_t **pixels;
+  void **to_free;
+} Image;
+
+typedef struct {
   enum ImageType imageType;
-  void *image;
+  Image image;
 } Context;
 
 bool context_load_image(Context *context, const char *file_path, enum ImageType type);
