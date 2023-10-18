4b7c2590b926e968cff914595f18203eaec620a2
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-18 17:58:04 +0200

    Displaying image 💀

--- image_gui_gtk/src/app.cpp
+++ image_gui_gtk/src/app.cpp
@@ -8,13 +8,30 @@ extern "C" {
 }
 
 ImageReaderApp::ImageReaderApp() {
-  load_button = Gtk::Button{"Load image"};
+  Gtk::Box container{};
+
+  load_button = Gtk::Button{"Load imageMat"};
   load_button.signal_clicked().connect(sigc::mem_fun(*this, &ImageReaderApp::handle_load_button_click));
-  set_child(load_button);
+  container.append(load_button);
+
+  image = Gtk::Image{};
+  container.append(image);
+  image.set_vexpand();
+  image.set_vexpand_set();
+
+  set_size_request(1280, 720);
+  set_child(container);
+
+  to_free = new void *;
+  *to_free = nullptr;
 }
 
 ImageReaderApp::~ImageReaderApp() {
-  delete image;
+  delete imageMat;
+  if (*to_free != nullptr) {
+    free(*to_free);
+  }
+  delete to_free;
 }
 
 void ImageReaderApp::handle_load_button_click() {
@@ -32,6 +49,12 @@ void ImageReaderApp::handle_load_button_click() {
   filter_ppm->set_name("PPM files");
   filter_ppm->add_pattern("*.ppm");
   dialog->add_filter(filter_ppm);
+
+  auto filter_jpeg = Gtk::FileFilter::create();
+  filter_jpeg->set_name("JPEG files");
+  filter_jpeg->add_pattern("*.jpg");
+  filter_jpeg->add_pattern("*.jpeg");
+  dialog->add_filter(filter_jpeg);
 
   auto filter_any = Gtk::FileFilter::create();
   filter_any->set_name("Any files");
@@ -57,21 +80,43 @@ void ImageReaderApp::on_file_dialog_response(int response_id, Gtk::FileChooserDi
       return;
     }
 
+    if (*to_free != nullptr) {
+      free(*to_free);
+    }
+
+    *to_free = read_image;
     newImage = cv::Mat{read_image->header.height, read_image->header.width, CV_8UC3, read_image->pixels};
-    free(read_image);
   } else {
     newImage = cv::Mat{cv::imread(file_path, cv::IMREAD_COLOR)};
   }
 
+  image_draw(newImage);
+
+  delete dialog;
+}
+
+void ImageReaderApp::image_draw(const cv::Mat &newImage) {
   // scale to HD
   double scale = std::min(
       (double) 1280 / newImage.size().width,
       (double) 720 / newImage.size().height
   );
 
-  delete image;
-  image = new cv::Mat;
-  cv::resize(newImage, *image, cv::Size(), scale, scale, cv::INTER_AREA);
+  delete imageMat;
+  imageMat = new cv::Mat;
+  cv::resize(newImage, *imageMat, cv::Size(), scale, scale, cv::INTER_AREA);
 
-  delete dialog;
+  const auto pixbuf =
+      Gdk::Pixbuf::create_from_data(
+          imageMat->data,
+          Gdk::Colorspace::RGB,
+          false,
+          8,
+          imageMat->size().width,
+          imageMat->size().height,
+          imageMat->size().width * sizeof(uint8_t) * 3
+      );
+
+  this->image.set(pixbuf);
+  this->image.set_size_request(1000, 720);
 }
