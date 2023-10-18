df599b50f1e55e5a6b96a76263c9d3831bab01d4
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-18 23:15:06 +0200

    Filling container with image

--- image_gui_gtk/include/image_gui_gtk/app.hpp
+++ image_gui_gtk/include/image_gui_gtk/app.hpp
@@ -5,7 +5,8 @@
 #include "gtkmm/window.h"
 #include "gtkmm/filechooserdialog.h"
 #include "gtkmm/button.h"
-#include "gtkmm/image.h"
+#include "gtkmm/picture.h"
+#include "ImageWrapper.hpp"
 
 class ImageReaderApp : public Gtk::Window {
  public:
@@ -13,15 +14,13 @@ class ImageReaderApp : public Gtk::Window {
   ~ImageReaderApp() override;
 
  private:
-  void **to_free;
-
-  cv::Mat *imageMat = nullptr;
+  ImageWrapper *imageWrapper_ = nullptr;
 
   Gtk::Button load_button;
   void handle_load_button_click();
 
   void on_file_dialog_response(int response_id, Gtk::FileChooserDialog *dialog);
 
-  Gtk::Image image;
-  void image_draw(const cv::Mat& newImage);
+  Gtk::Picture picture;
+  void image_draw(ImageWrapper *newImage);
 };
