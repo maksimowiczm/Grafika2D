d1123f280079dddf352e709634b5d398e3e19957
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-18 17:58:04 +0200

    Displaying image 💀

--- image_gui_gtk/include/image_gui_gtk/app.hpp
+++ image_gui_gtk/include/image_gui_gtk/app.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "opencv2/core/mat.hpp"
+
 #include "gtkmm/window.h"
 #include "gtkmm/filechooserdialog.h"
-#include "opencv2/core/mat.hpp"
 #include "gtkmm/button.h"
+#include "gtkmm/image.h"
 
 class ImageReaderApp : public Gtk::Window {
  public:
@@ -11,10 +13,15 @@ class ImageReaderApp : public Gtk::Window {
   ~ImageReaderApp() override;
 
  private:
-  cv::Mat *image = nullptr;
+  void **to_free;
+
+  cv::Mat *imageMat = nullptr;
 
   Gtk::Button load_button;
   void handle_load_button_click();
 
   void on_file_dialog_response(int response_id, Gtk::FileChooserDialog *dialog);
+
+  Gtk::Image image;
+  void image_draw(const cv::Mat& newImage);
 };
