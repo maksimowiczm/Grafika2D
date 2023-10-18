7e864d350c8f235a38be118b1f1df1e728025ae8
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-18 17:22:04 +0200

    Loading opencv mat

--- image_gui_gtk/include/image_gui_gtk/app.hpp
+++ image_gui_gtk/include/image_gui_gtk/app.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
-#include <gtkmm/window.h>
+#include "gtkmm/window.h"
+#include "gtkmm/filechooserdialog.h"
+#include "opencv2/core/mat.hpp"
 #include "gtkmm/button.h"
 
 class ImageReaderApp : public Gtk::Window {
  public:
   ImageReaderApp();
   ~ImageReaderApp() override;
+
+ private:
+  cv::Mat *image = nullptr;
+
+  Gtk::Button load_button;
+  void handle_load_button_click();
+
+  void on_file_dialog_response(int response_id, Gtk::FileChooserDialog *dialog);
 };
