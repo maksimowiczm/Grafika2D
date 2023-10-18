0ca2a78be09c51d22b501f13e59f1a2787329716
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-18 16:14:56 +0200

    Gtkmm application

--- src/unix-main.cpp
+++ src/unix-main.cpp
@@ -1,17 +1,8 @@
-#include "gtk/gtk.h"
+#include "gtkmm/application.h"
 
-extern "C" {
-#include "image_gui_gtk/app.h"
-}
-
-int gtk_app_start(int argc, char *argv[]) {
-  GtkApplication *app = gtk_application_new("org.gtk.grafika", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(gui_gtk_app_activate), NULL);
-  int status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-  return status;
-}
+#include "image_gui_gtk/app.hpp"
 
 int main(int argc, char *argv[]) {
-  return gtk_app_start(argc, argv);
+  auto app = Gtk::Application::create("org.gtkmm.grafika");
+  return app->make_window_and_run<ImageReaderApp>(argc, argv);
 }
