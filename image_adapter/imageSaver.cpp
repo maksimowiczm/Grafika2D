175c22f6dee21e470a974ba96a0f3e74cd5017ef
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-19 12:08:24 +0200

    Applying jpeg extension on save

--- image_adapter/imageSaver.cpp
+++ image_adapter/imageSaver.cpp
@@ -2,12 +2,16 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 
-void ImageSaver::save_jpeg(const char *file_path, const cv::Mat &image, int jpeg_quality) {
+void ImageSaver::save_jpeg(const std::string &file_path, const cv::Mat &image, int jpeg_quality) {
   std::vector<int> writeParams{};
   writeParams.push_back(cv::IMWRITE_JPEG_QUALITY);
   writeParams.push_back(jpeg_quality);
 
+  std::string path{file_path};
+  if (!file_path.ends_with(".jpeg")) {
+    path += ".jpeg";
+  }
   cv::Mat out;
   cv::cvtColor(image, out, cv::COLOR_BGR2RGB);
-  cv::imwrite(file_path, out, writeParams);
+  cv::imwrite(path, out, writeParams);
 }
