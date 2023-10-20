594968a8b5aad21bf8a523a3fb36110e8c8c1190
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-20 20:47:32 +0200

    Updating color values 💀

--- gtk-ui/src/internal/colors_context.c
+++ gtk-ui/src/internal/colors_context.c
@@ -3,6 +3,8 @@
 #include "stdlib.h"
 #include "string.h"
 #include "color_converter/cmyk_converter.h"
+#include "color_converter/rgb_converter.h"
+#include "shared/uint8.h"
 
 ColorsContext *singleton_context(ColorsContext *context) {
   static ColorsContext *singleton;
@@ -27,7 +29,12 @@ ColorsContext *colors_context_new() {
   context->image = malloc(sizeof(GtkImage *));
   context->pixbuf = malloc(sizeof(GdkPixbuf *));
 
+  context->inputs = malloc(sizeof(GtkText *) * 7);
+  context->scales = malloc(sizeof(GtkScale *) * 7);
+
   singleton_context(context);
+
+  context->RGB_MODE = false;
 
   return context;
 }
@@ -49,12 +56,16 @@ void colors_context_color_update(ColorsContext *context, enum ColorMode mode) {
   if (context == NULL) {
     return;
   }
+  if ((context->RGB_MODE && mode == CMYK) || (!context->RGB_MODE && mode == RGB)) {
+    return;
+  }
+
   guchar buffer[3];
-  if (mode == RGB) {
+  if (context->RGB_MODE) {
     buffer[0] = (*context->rgb)->red;
     buffer[1] = (*context->rgb)->green;
     buffer[2] = (*context->rgb)->blue;
-  } else if (mode == CMYK) {
+  } else {
     ColorCmyk *contextCmyk = *context->cmyk;
     ColorRgb rgb = color_converter_cmyk2rgb(*contextCmyk);
 
@@ -76,4 +87,40 @@ void colors_context_color_update(ColorsContext *context, enum ColorMode mode) {
   );
   pixbuf = gdk_pixbuf_scale_simple(pixbuf, 3840, 2160, GDK_INTERP_NEAREST);
   gtk_picture_set_pixbuf(*context->image, pixbuf);
+
+  if (context->RGB_MODE) {
+    ColorRgb *rgb = *context->rgb;
+    ColorCmyk cmyk = color_converter_rgb2cmyk(*rgb);
+    (*context->cmyk)->cyan = cmyk.cyan;
+    (*context->cmyk)->magenta = cmyk.magenta;
+    (*context->cmyk)->yellow = cmyk.yellow;
+    (*context->cmyk)->key = cmyk.key;
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[CYAN]), uint8_to_string(cmyk.cyan), 3);
+    gtk_range_set_value(context->scales[CYAN], cmyk.cyan);
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[MAGENTA]), uint8_to_string(cmyk.magenta), 3);
+    gtk_range_set_value(context->scales[MAGENTA], cmyk.magenta);
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[YELLOW]), uint8_to_string(cmyk.yellow), 3);
+    gtk_range_set_value(context->scales[YELLOW], cmyk.yellow);
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[KEY]), uint8_to_string(cmyk.key), 3);
+    gtk_range_set_value(context->scales[KEY], cmyk.key);
+  } else {
+    ColorCmyk *cmyk = *context->cmyk;
+    ColorRgb rgb = color_converter_cmyk2rgb(*cmyk);
+    (*context->rgb)->red = rgb.red;
+    (*context->rgb)->green = rgb.green;
+    (*context->rgb)->blue = rgb.blue;
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[RED]), uint8_to_string(rgb.red), 3);
+    gtk_range_set_value(context->scales[RED], rgb.red);
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[GREEN]), uint8_to_string(rgb.green), 3);
+    gtk_range_set_value(context->scales[GREEN], rgb.green);
+
+    gtk_entry_buffer_set_text(gtk_text_get_buffer(context->inputs[BLUE]), uint8_to_string(rgb.blue), 3);
+    gtk_range_set_value(context->scales[BLUE], rgb.blue);
+  }
 }
--- gtk-ui/src/internal/rgb_container.c
+++ gtk-ui/src/internal/rgb_container.c
@@ -5,9 +5,9 @@ GtkWidget *rgb_container_new(ColorsContext *context) {
   GtkWidget *container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 
   ColorRgb *rgb = *context->rgb;
-  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("red", &rgb->red, 255, RGB));
-  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("green", &rgb->green, 255, RGB));
-  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("blue", &rgb->blue, 255, RGB));
+  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("red", &rgb->red, 255, RGB, RED));
+  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("green", &rgb->green, 255, RGB, GREEN));
+  gtk_box_append(GTK_BOX(container), gtk_ui_uint8_picker("blue", &rgb->blue, 255, RGB, BLUE));
 
   return container;
 }
