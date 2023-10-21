462e5b658e40ccd4c8f57995437bd24d8ad472fb
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-21 17:36:40 +0200

    Rotating rgb cube

--- include/cube_state/cube_context.h
+++ include/cube_state/cube_context.h
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "cube_state.h"
+#include "cube_context.h"
+#include "gtk/gtk.h"
 
 typedef struct cube_context CubeContext;
 
 struct cube_context {
-
+  GtkPicture **picture;
+  ICubeState state;
 };
+
+CubeContext *cube_context_new(GtkPicture *picture);
+
+void cube_context_free(CubeContext *context);
+
+void cube_context_left(CubeContext *context);
+void cube_context_right(CubeContext *context);
+void cube_context_up(CubeContext* context);
+void cube_context_down(CubeContext* context);
