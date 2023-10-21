8eccbcd140de33aeee968a7888337d4981e0e0ed
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-21 17:09:43 +0200

    Rgb cube states

---
+++ include/cube_state/cube_context.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "cube_state.h"
+
+typedef struct cube_context CubeContext;
+
+struct cube_context {
+
+};
---
+++ src/cube_state/cube_state.c
@@ -0,0 +1,78 @@
+#include "cube_state/cube_state.h"
+#include "cube.h"
+
+ICubeState cube_state_get_cube_state(enum CubeState type) {
+  switch (type) {
+    case Front: return cube_state_front_get();
+    case Top: return cube_state_top_get();
+    case Side: return cube_state_side_get();
+    case FrontPrime: return cube_state_prime_front_get();
+    case TopPrime: return cube_state_prime_top_get();
+    case SidePrime: return cube_state_prime_side_get();
+  }
+}
+
+#define STATE_NEW(NAME, UP, RIGHT, DOWN, LEFT, PIXELS) \
+ICubeState NAME() {                                    \
+  ICubeState state;                                    \
+  state.up = UP;                                       \
+  state.left = LEFT;                                   \
+  state.right = RIGHT;                                 \
+  state.down = DOWN;                                   \
+  state.get_pixels = PIXELS;                           \
+  return state;                                        \
+}
+
+STATE_NEW(
+    cube_state_front_get,
+    cube_state_top_get,
+    cube_state_side_get,
+    cube_state_prime_top_get,
+    cube_state_prime_side_get,
+    get_front_wall
+)
+
+STATE_NEW(
+    cube_state_prime_front_get,
+    cube_state_prime_top_get,
+    cube_state_side_get,
+    cube_state_top_get,
+    cube_state_prime_side_get,
+    get_prime_top_wall
+)
+
+STATE_NEW(
+    cube_state_top_get,
+    cube_state_prime_front_get,
+    cube_state_side_get,
+    cube_state_front_get,
+    cube_state_prime_side_get,
+    get_top_wall
+)
+
+STATE_NEW(
+    cube_state_prime_top_get,
+    cube_state_front_get,
+    cube_state_side_get,
+    cube_state_prime_front_get,
+    cube_state_prime_side_get,
+    get_prime_top_wall
+)
+
+STATE_NEW(
+    cube_state_side_get,
+    cube_state_top_get,
+    cube_state_prime_front_get,
+    cube_state_prime_top_get,
+    cube_state_front_get,
+    get_side_wall
+)
+
+STATE_NEW(
+    cube_state_prime_side_get,
+    cube_state_top_get,
+    cube_state_front_get,
+    cube_state_prime_top_get,
+    cube_state_prime_front_get,
+    get_prime_side_wall
+)
