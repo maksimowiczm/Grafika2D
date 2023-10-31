3b528e9ef971b64339c6b3c5ef3f48327e984a69
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-31 16:19:28 +0100

    Cv::Mat image operations

---
+++ src/operations/mod.rs
@@ -0,0 +1,38 @@
+use opencv::{self as cv, prelude::*};
+use crate::channels::Channels;
+
+pub trait Operations {
+    fn add_brightness(&mut self, value: i16) -> Result<(), opencv::Error>;
+    fn add_to_channel(&mut self, value: i16, channel: usize) -> Result<(), opencv::Error>;
+}
+
+fn no_overflow_add_u8(number: u8, to_add: i16) -> u8 {
+    let mut new = number as i16 + to_add;
+    if new > u8::MAX as i16 {
+        new = u8::MAX as i16;
+    } else if new < u8::MIN as i16 {
+        new = u8::MIN as i16;
+    }
+    return new as u8;
+}
+
+impl Operations for Mat {
+    fn add_brightness(&mut self, value: i16) -> Result<(), opencv::Error> {
+        self
+            .data_bytes_mut()?
+            .into_iter()
+            .for_each(|b| *b = no_overflow_add_u8(*b, value));
+
+        Ok(())
+    }
+
+    fn add_to_channel(&mut self, value: i16, channel: usize) -> Result<(), cv::Error> {
+        let channels = self.channels();
+        self
+            .data_bytes_mut()?
+            .get_channel(channels as usize, channel)
+            .for_each(|b| *b = no_overflow_add_u8(*b, value));
+
+        Ok(())
+    }
+}
