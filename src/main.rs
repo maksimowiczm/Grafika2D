89a0d688ce8b382746557b64254e1ad906b14411
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-01 10:15:57 +0100

    Filter buttons

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ mod operations;
 mod channels;
 mod image;
 
+use opencv::{prelude::*};
 
+use std::cell::RefCell;
+use std::error::Error;
+use std::rc::Rc;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
-use gtk::gdk_pixbuf::Colorspace::Rgb;
 use gtk::gdk_pixbuf::Pixbuf;
 use glib::Bytes;
+use crate::filters::Filters;
 use crate::image::Image;
+
+struct Context {
+    mat: Option<Mat>,
+}
 
 const APP_ID: &str = "org.grafika.rust.obrazy";
 
@@ -30,30 +38,31 @@ fn build_ui(app: &Application) {
         .build();
 
     let picture = gtk::Picture::new();
+    let context = Rc::new(RefCell::new(Context { mat: None }));
 
-    main_container.append(&build_actions_container(window.clone(), picture.clone()));
+    main_container.append(&build_actions_container(&window, picture.clone(), Rc::clone(&context)));
     main_container.append(&picture);
 
     window.present();
 }
 
-fn build_actions_container(window: ApplicationWindow, picture: gtk::Picture) -> gtk::Box {
+fn build_actions_container(window: &ApplicationWindow, picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Box {
     let actions_container = gtk::Box::new(gtk::Orientation::Vertical, 10);
-    actions_container.append(&build_load_button(window.clone(), picture.clone()));
+    actions_container.append(&build_load_button(window.clone(), picture.clone(), Rc::clone(&context)));
+    actions_container.append(&build_filters_container(picture.clone(), Rc::clone(&context)));
     actions_container
 }
 
-fn build_load_button(window: ApplicationWindow, picture: gtk::Picture) -> gtk::Button
-{
+fn build_load_button(window: ApplicationWindow, picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Button {
     let button = gtk::Button::with_label("Load image");
-    button.connect_clicked(move |_| build_file_chooser(window.clone(), picture.clone()).show());
+    button.connect_clicked(move |_| build_file_chooser(&window, picture.clone(), Rc::clone(&context)).show());
     button
 }
 
-fn build_file_chooser(window: ApplicationWindow, picture: gtk::Picture) -> gtk::FileChooserDialog {
+fn build_file_chooser(window: &ApplicationWindow, picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::FileChooserDialog {
     let chooser = gtk::FileChooserDialog::new(
         Some("Load image"),
-        Some(&window),
+        Some(window),
         gtk::FileChooserAction::Open,
         &[("Open", gtk::ResponseType::Ok), ("Cancel", gtk::ResponseType::Cancel)],
     );
@@ -78,16 +87,8 @@ fn build_file_chooser(window: ApplicationWindow, picture: gtk::Picture) -> gtk::
         if let Some(file) = dialog.file() {
             if let Some(path) = file.path() {
                 if let Ok(out) = image::load_image(path.to_str().unwrap()) {
-                    let (width, height, channels, pixels) = out.destruct_mat().unwrap();
-                    let bytes = Bytes::from(&pixels);
-                    let pixbuf = Pixbuf::from_bytes(&bytes,
-                                                    Rgb,
-                                                    false,
-                                                    8,
-                                                    width as i32,
-                                                    height as i32,
-                                                    width as i32 * channels as i32);
-                    picture.set_pixbuf(Some(pixbuf.as_ref()));
+                    context.borrow_mut().mat = Some(out.clone());
+                    picture_update_pixbuf(&picture, &out);
                     dialog.destroy();
                 } else {
                     handle_error();
@@ -101,4 +102,47 @@ fn build_file_chooser(window: ApplicationWindow, picture: gtk::Picture) -> gtk::
     });
 
     chooser
+}
+
+fn build_filters_container(picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Box {
+    let filters_container = gtk::Box::new(gtk::Orientation::Vertical, 5);
+
+    let filters: Vec<(&str, fn(&mut Mat) -> Result<(), Box<dyn Error>>)> = vec!(
+        ("Mean", |mat| mat.mean_filter(3)),
+        ("High pass", |mat| mat.high_pass_filter(3)),
+        ("Sobel", Filters::sobel_filter),
+        ("Gauss", Filters::gauss_filter),
+    );
+
+    filters.iter().for_each(|(label, func)| {
+        let btn = build_filter_button(label, picture.clone(), Rc::clone(&context), *func);
+        filters_container.append(&btn)
+    });
+
+    filters_container
+}
+
+fn build_filter_button(label: &str, picture: gtk::Picture, context: Rc<RefCell<Context>>, method: fn(&mut Mat) -> Result<(), Box<dyn Error>>) -> gtk::Button {
+    let button = gtk::Button::with_label(label);
+    button.connect_clicked(move |_| {
+        if let Some(mat) = &mut context.borrow_mut().mat {
+            method(mat).unwrap();
+            picture_update_pixbuf(&picture, &mat);
+        }
+    });
+    button
+}
+
+fn picture_update_pixbuf(picture: &gtk::Picture, mat: &Mat) {
+    let (width, height, channels, pixels) = mat.destruct_mat().unwrap();
+    let bytes = Bytes::from(&pixels);
+    let pixbuf = Pixbuf::from_bytes(&bytes,
+                                    gtk::gdk_pixbuf::Colorspace::Rgb,
+                                    false,
+                                    8,
+                                    width as i32,
+                                    height as i32,
+                                    width as i32 * channels as i32);
+
+    picture.set_pixbuf(Some(pixbuf.as_ref()));
 }
