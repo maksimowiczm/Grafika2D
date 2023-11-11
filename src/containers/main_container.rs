69ac3b805fbc223652173dfcb49d6b30f09ba498
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-11 18:36:05 +0100

    Reset image in main container

--- src/containers/main_container.rs
+++ src/containers/main_container.rs
@@ -11,11 +11,24 @@ use crate::context::{Context, picture_update_pixbuf};
 pub fn build_main_container(window: &ApplicationWindow, picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Box {
     let main_container = gtk::Box::new(gtk::Orientation::Vertical, 10);
     main_container.append(&build_load_button(window.clone(), picture.clone(), Rc::clone(&context)));
+    main_container.append(&build_reset_button(picture.clone(), Rc::clone(&context)));
 
     // image actions
     main_container.append(&build_actions_container(picture.clone(), Rc::clone(&context)));
 
     main_container
+}
+
+fn build_reset_button(picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Button {
+    let button = gtk::Button::with_label("Reset image");
+    button.connect_clicked(move |_| {
+        let mut c_context = context.borrow_mut();
+        if let Some(mat) = &c_context.starting {
+            picture_update_pixbuf(&picture, &mat);
+            c_context.mat = Some(mat.clone());
+        }
+    });
+    button
 }
 
 fn build_load_button(window: ApplicationWindow, picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Button {
