cb40298cd225728350881b163157f7fa031cf5f8
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-12 12:08:28 +0100

    Black selection button

--- src/containers/binarization_container.rs
+++ src/containers/binarization_container.rs
@@ -1,8 +1,8 @@
+use crate::binarization::Binarization;
+use crate::context::{picture_update_pixbuf, Context};
+use gtk::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
-use gtk::prelude::*;
-use crate::binarization::Binarization;
-use crate::context::{Context, picture_update_pixbuf};
 
 pub fn build_binarization_container(picture: gtk::Picture, context: Rc<RefCell<Context>>) -> gtk::Box {
     let container = gtk::Box::new(gtk::Orientation::Vertical, 5);
@@ -20,7 +20,8 @@ pub fn build_binarization_container(picture: gtk::Picture, context: Rc<RefCell<C
 
     container.append(&binarize_container);
 
-    container.append(&build_binarize_button(picture, context, threshold_entry));
+    container.append(&build_binarize_button(picture, Rc::clone(&context), threshold_entry.clone()));
+    container.append(&build_percent_black_selection_button(context, threshold_entry));
 
     container
 }
@@ -40,4 +41,33 @@ fn build_binarize_button(picture: gtk::Picture, context: Rc<RefCell<Context>>, t
         });
 
     button
+}
+
+fn build_percent_black_selection_button(context: Rc<RefCell<Context>>, threshold_entry: gtk::Entry) -> gtk::Box {
+    let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let label = gtk::Label::with_mnemonic("Percent black selection");
+    container.append(&label);
+
+    let entry_container = gtk::Box::new(gtk::Orientation::Horizontal, 1);
+    entry_container.append(&gtk::Label::with_mnemonic("Percent"));
+    let entry = gtk::Entry::new();
+    entry_container.append(&entry);
+    container.append(&entry_container);
+
+    let button = gtk::Button::with_label("Get threshold");
+    container.append(&button);
+
+    button.connect_clicked(move |_| {
+        if let Some(mat) = &mut context.borrow_mut().mat {
+            if let Ok(percent) = entry.buffer().text().parse::<f64>() {
+                if percent > 100. || percent < 0. {
+                    return;
+                }
+                let threshold = mat.percent_black_selection(percent / 100.).unwrap();
+                threshold_entry.buffer().set_text(format!("{threshold}"));
+            }
+        }
+    });
+
+    container
 }
