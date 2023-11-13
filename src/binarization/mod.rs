0d502c3a45aa5fd0796f553852caee248a8599e2
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-13 17:41:59 +0100

    Mean iterative selection binarization

--- src/binarization/mod.rs
+++ src/binarization/mod.rs
@@ -1,12 +1,13 @@
 use std::error::Error;
 use opencv::core::Mat;
 use opencv::prelude::*;
-use crate::histogram::Histogram;
+use crate::histogram::{Histogram};
 use crate::operations::{no_overflow_add_u8, Operations};
 
 pub trait Binarization {
     fn threshold_binarization(&mut self, threshold: u8) -> Result<(), Box<dyn Error>>;
     fn percent_black_selection(&mut self, percent: f64) -> Result<u8, Box<dyn Error>>;
+    fn mean_iterative_selection(&mut self) -> Result<u8, Box<dyn Error>>;
 }
 
 impl Binarization for Mat {
@@ -52,5 +53,35 @@ impl Binarization for Mat {
             .len();
 
         Ok(no_overflow_add_u8(threshold as u8, 1))
+    }
+
+    fn mean_iterative_selection(&mut self) -> Result<u8, Box<dyn Error>> {
+        let mut threshold = self.percent_black_selection(0.25).unwrap() as usize;
+        let histogram = self.get_histogram();
+
+        loop {
+            let (tbu, tbd) = histogram
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i < threshold)
+                .fold((0, 0), |(u, d), (i, row)| (u + i * row.len(), d + row.len()));
+            let tb = tbu as f64 / tbd as f64;
+
+            let (twu, twd) = histogram
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i >= threshold)
+                .fold((0, 0), |(u, d), (i, row)| (u + i * row.len(), d + row.len()));
+            let tw = twu as f64 / twd as f64;
+
+            let prev_threshold = threshold;
+            threshold = ((tb + tw) / 2.) as usize;
+
+            if threshold == prev_threshold {
+                break;
+            }
+        }
+
+        Ok(threshold as u8)
     }
 }
